a1a75509191ccf66773944269b6bd12b5005589b
Sanjev2 <sanjeevmanandhar53@gmail.com>
2025-12-28 19:27:55 +0545

    commiited

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  swcMinify: true, // optional but recommended
 };
 
 module.exports = nextConfig;
